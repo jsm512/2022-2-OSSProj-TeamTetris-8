3cfbd65b222264d0a4f16972adaf726c2301e65d
jsm512 <wlstmdals12@naver.com>
2022-11-27 01:18:15 +0900

    feat:volum_setting page font size

--- Tetris_Game/Tetris.py
+++ Tetris_Game/Tetris.py
@@ -1571,6 +1571,7 @@ pygame.time.set_timer(pygame.USEREVENT, 500)
 ###########################################################
 
 while not done:
+        
     # 게임안에서 Pause 눌렀을 때 screen
     if pause:
         pygame.mixer.music.pause()
@@ -1717,20 +1718,25 @@ while not done:
                         pygame.time.set_timer(pygame.USEREVENT, framerate)
                     else:
                         pygame.time.set_timer(pygame.USEREVENT, game_speed)
+                draw1_mino(dx, dy, mino_en, rotation, matrix)
+                draw_image(screen, gamebackground_image, board_width * 0.5, board_height *
+                           0.5, board_width, board_height)  # (window, 이미지주소, x좌표, y좌표, 너비, 높이)
+                draw1_board(next_mino1_en, next_mino2_en,
+                           hold_mino, score, level, goal)
 
                 # Draw a mino 5 * 10
-                if board_height <= 450 and board_width <= 800:
-                    draw1_mino(dx, dy, mino_en, rotation, matrix)
-                    draw_image(screen, gamebackground_image, board_width * 0.5, board_height *
-                           0.5, board_width, board_height)  # (window, 이미지주소, x좌표, y좌표, 너비, 높이)
-                    draw1_board(next_mino1_en, next_mino2_en,
-                           hold_mino, score, level, goal)
+                # if board_height <= 225 and board_width <= 400:
+                #     draw1_mino(dx, dy, mino_en, rotation, matrix)
+                #     draw_image(screen, gamebackground_image, board_width * 0.5, board_height *
+                #            0.5, board_width, board_height)  # (window, 이미지주소, x좌표, y좌표, 너비, 높이)
+                #     draw1_board(next_mino1_en, next_mino2_en,
+                #            hold_mino, score, level, goal)
                 #10*20
-                elif board_height <= 675 and board_width <= 1200:
-                    draw_mino(dx, dy, mino, rotation, matrix)
-                    draw_mino(dx_2P, dy_2P, mino_2P, rotation_2P, matrix_2P)
-                    draw_multiboard(next_mino1, hold_mino, next_mino1_2P,
-                                    hold_mino_2P, current_key, current_key_2P)
+                # elif board_height <= 450 and board_width <= 800:
+                #     draw_mino(dx, dy, mino, rotation, matrix)
+                #     draw_mino(dx_2P, dy_2P, mino_2P, rotation_2P, matrix_2P)
+                #     draw_multiboard(next_mino1, hold_mino, next_mino1_2P,
+                #                     hold_mino_2P, current_key, current_key_2P)
                 #15 * 30
                 # elif 1600 900    
                 
@@ -3805,18 +3811,18 @@ while not done:
         back_button.draw(screen, (0, 0, 0))
 
         # render("텍스트이름", 안티에일리어싱 적용, 색깔), 즉 아래의 코드에서 숫자 1=안티에일리어싱 적용에 관한 코드
-        music_volume_text = ui_variables.h5.render(
+        music_volume_text = ui_variables.h6.render(
             'Music Volume', 1, ui_variables.white)
-        effect_volume_text = ui_variables.h5.render(
+        effect_volume_text = ui_variables.h6.render(
             'Effect Volume', 1, ui_variables.white)
         screen.blit(music_volume_text, (board_width *
                     0.3, board_height * 0.4))  # 위치 비율 고정
         screen.blit(effect_volume_text, (board_width *
                     0.3, board_height * 0.6))  # 위치 비율 고정
 
-        music_volume_text = ui_variables.h5.render(
+        music_volume_text = ui_variables.h6.render(
             'Music On/Off', 1, ui_variables.white)
-        effect_volume_text = ui_variables.h5.render(
+        effect_volume_text = ui_variables.h6.render(
             'Effect On/Off', 1, ui_variables.white)
         screen.blit(music_volume_text, (board_width *
                     0.5, board_height * 0.4))  # 위치 비율 고정
@@ -4276,7 +4282,6 @@ while not done:
                 if board_width < min_width or board_height < min_height:  # 최소 너비 또는 높이를 설정하려는 경우
                     board_width = min_width
                     board_height = min_height
-                    pygame.display.set_caption("TETRIS(s)")
                 # 높이 또는 너비가 비율의 일정수준 이상을 넘어서게 되면
                 if not ((board_rate - 0.1) < (board_height / board_width) < (board_rate + 0.05)):
                     # 너비를 적정 비율로 바꿔줌
